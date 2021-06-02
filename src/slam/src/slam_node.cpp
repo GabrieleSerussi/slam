d40f31fbceb86a124a0d3076f11c8896fcda2208
GabrieleSerussi <serussigabriele@gmail.com>
2021-06-03 00:34:15 +0200

    bug fix

--- src/slam/src/slam_node.cpp
+++ src/slam/src/slam_node.cpp
@@ -87,8 +87,8 @@ void tagCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr msg) {
           try{
             transformStamped = tfBuffer.lookupTransform("odom","fisheye_rect", ros::Time(0), ros::Duration(3.0));
             // devo mettere la rotazione della trasformata nella trasformata e dopo moltiplicare la trasformata per il punto fornito da apriltag
-            //tf2::Quaternion q = tf2::Quaternion(transformStamped.transform.rotation.x, transformStamped.transform.rotation.y, transformStamped.transform.rotation.z, transformStamped.transform.rotation.w);
-            tf2::Quaternion q; tf2::convert(transformStamped.transform.rotation, q);
+            tf2::Quaternion q = tf2::Quaternion(transformStamped.transform.rotation.x, transformStamped.transform.rotation.y, transformStamped.transform.rotation.z, transformStamped.transform.rotation.w);
+            //tf2::Quaternion q; tf2::convert(transformStamped.transform.rotation, q);
             tf2::Vector3 translation = tf2::Vector3(transformStamped.transform.translation.x, transformStamped.transform.translation.y, transformStamped.transform.translation.z);
             tf2::Transform transf_camera2odom = tf2::Transform(q, translation);
 
